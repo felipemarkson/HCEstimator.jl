07e31ad37a3c95e6b9464fc4e1457f3747dcd189
Felipe M. dos S. Monteiro <fmarkson@outlook.com>
2022-03-03 19:33:48 -0300

    Add auxiliary function in Get module

--- src/DistSystem.jl
+++ src/DistSystem.jl
@@ -1,6 +1,6 @@
 module DistSystem
 
-export Substation, DG, System
+export Substation, DG, System, Get
 
 include("Tools.jl")
 using .Tools
@@ -62,7 +62,7 @@ function factory_system(data::DataFrame, VL::Float64, VH::Float64, sub::Substati
     sys.PL = data.P_MW[1:sys.nbuses]
     sys.QL = data.Q_MVAr[1:sys.nbuses]
     sys.dgs = []
-    sys.Bsh =  (- (data.Bshunt_MVAr*1e6)./(sub.nominal_voltage^2))/Yᴺ
+    sys.Bsh = (-(data.Bshunt_MVAr * 1e6) ./ (sub.nominal_voltage^2)) / Yᴺ
 
     sys.substation = sub
 
@@ -70,4 +70,18 @@ function factory_system(data::DataFrame, VL::Float64, VH::Float64, sub::Substati
     return sys
 
 end
+
+module Get
+using JuMP: value
+export voltage, current, power, current_bsh, current_ij, power_dg, losses_ij, losses
+voltage(model, bus, l_scenario, type_dg_hc, scenario_hc) = value.(model[:Vre])[bus, l_scenario, type_dg_hc, scenario_hc] + 1im * value.(model[:Vim])[bus, l_scenario, type_dg_hc, scenario_hc]
+current(model, bus, l_scenario, type_dg_hc, scenario_hc) = value.(model[:Ire])[bus, l_scenario, type_dg_hc, scenario_hc] + 1im * value.(model[:Iim])[bus, l_scenario, type_dg_hc, scenario_hc]
+power(model, bus, l_scenario, type_dg_hc, scenario_hc) = value.(model[:P])[bus, l_scenario, type_dg_hc, scenario_hc] + 1im * value.(model[:Q])[bus, l_scenario, type_dg_hc, scenario_hc]
+current_bsh(model, bus, l_scenario, type_dg_hc, scenario_hc) = value.(model[:I_bsh_re])[bus, l_scenario, type_dg_hc, scenario_hc] + 1im * value.(model[:I_bsh_re])[bus, l_scenario, type_dg_hc, scenario_hc]
+current_ij(model, i, j, l_scenario, type_dg_hc, scenario_hc) = value.(model[:Iijre])[i, j, l_scenario, type_dg_hc, scenario_hc] + 1im * value.(model[:Iijim])[i, j, l_scenario, type_dg_hc, scenario_hc]
+power_dg(model, nº, l_scenario, type_dg_hc, scenario_hc) = value.(model[:Pdg])[nº, l_scenario, type_dg_hc, scenario_hc] + 1im * value.(model[:Pdg])[nº, l_scenario, type_dg_hc, scenario_hc]
+losses_ij(model, i, j, l_scenario, type_dg_hc, scenario_hc) = value.(model[:Ploss_ij])[i, j, l_scenario, type_dg_hc, scenario_hc]
+losses(model, l_scenario, type_dg_hc, scenario_hc) = value.(model[:Ploss])[l_scenario, type_dg_hc, scenario_hc]
+end
+
 end
