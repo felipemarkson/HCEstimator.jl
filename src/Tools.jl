d438a13085c1225b61b1c466015012f96a28859b
Felipe M. dos S. Monteiro <fmarkson@outlook.com>
2022-03-08 16:39:16 -0300

    Fiz data Type and runFormatter

--- src/Tools.jl
+++ src/Tools.jl
@@ -11,11 +11,11 @@ function make_Y_bus(data, VN)
 
     n_bus = length(collect(skipmissing(data.Bus)))
     Y = zeros(Complex, n_bus, n_bus)
-    Z = zeros(Complex, n_bus, n_bus) .+Inf
+    Z = zeros(Complex, n_bus, n_bus) .+ Inf
 
-    branch_data = data[data.R_Ohm.!==missing, [:FB, :TB, :R_Ohm, :X_ohm]]
+    branch_data = data[data.Type.=="Fixed", [:FB, :TB, :R_Ohm, :X_ohm]]
 
-    Bshunt = 1im*(data.Bshunt_MVAr*1e6)./(VN^2)
+    Bshunt = 1im * (data.Bshunt_MVAr * 1e6) ./ (VN^2)
 
     for value in eachrow(branch_data)
         Z[value.FB, value.TB] = value.R_Ohm + 1im * value.X_ohm
@@ -25,9 +25,9 @@ function make_Y_bus(data, VN)
     for i in 1:n_bus
         for j in 1:n_bus
             if i == j
-                Y[i, j] = sum(admitance.(Z[i,:])) + Bshunt[i]
+                Y[i, j] = sum(admitance.(Z[i, :])) + Bshunt[i]
             else
-                Y[i, j] = - admitance(Z[i,j])
+                Y[i, j] = -admitance(Z[i, j])
             end
         end
     end
@@ -35,5 +35,5 @@ function make_Y_bus(data, VN)
     return Y
 
 end
-  
+
 end
