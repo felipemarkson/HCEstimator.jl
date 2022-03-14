9684e01f3017468ec5ebe9e3bb3056489f5eb29b
Felipe M. dos S. Monteiro <fmarkson@outlook.com>
2022-03-14 14:49:48 -0300

    add tests cases

--- tests/test_Estimator.jl
+++ tests/test_Estimator.jl
@@ -20,7 +20,7 @@ function case3_dist()
     Q_limit = 10
     bus_sub = 1
     nbuses = 3
-    VL = 0.95
+    VL = 0.90
     VH = 1.05
     sub = DistSystem.Substation(VN, bus_sub, Vsub, P_limt, Q_limit, [0.003, 12, 240])
     data = DataFrame(CSV.File("tests/case3_dist.csv"))
@@ -28,18 +28,12 @@ function case3_dist()
     sys = DistSystem.factory_system(data, VL, VH, sub)
 
     sys.dgs = [
-        DistSystem.DG(3, 0.03, 0.0, [0.0026, 10.26, 210])
+        DistSystem.DER(3, 0.02, 0.05, [0.0, 0.03], [-0.03, 0.03], [0.0, 1.0], [0.0026, 10.26, 210]), #DG dispatchable
     ]
-    sys.m_load = collect(0.5:0.1:1.1)
-    sys.m_new_dg = [
-        collect(0.0:0.2:1.1)
-    ]
+    sys.m_load = [0.5, 0.8, 1.0]
+    sys.m_new_dg = [-1.0, 0.0, 1]
 
-
-
-
-
-    return sys, case
+    return sys, "case3_dist"
 end
 
 function case33()
@@ -56,21 +50,15 @@ function case33()
     sub = DistSystem.Substation(VN, bus_sub, Vsub, P_limt, Q_limit, [0.003, 12, 240])
     data = DataFrame(CSV.File("tests/case33.csv"))
     sys = DistSystem.factory_system(data, VL, VH, sub)
-    case = Case(
-        "case33",
-        [1.0000, 0.9973, 0.9848, 0.9785, 0.9723, 0.9583, 0.9563, 0.9520, 0.9475, 0.9435, 0.9428, 0.9416, 0.9383, 0.9378, 0.9376, 0.9375, 0.9396, 0.9403, 0.9968, 0.9932, 0.9925, 0.9919, 0.9813, 0.9746, 0.9713, 0.9568, 0.9547, 0.9468, 0.9412, 0.9386, 0.9379, 0.9382, 0.9398],
-        [0, -0.0150, -0.0934, -0.1490, -0.2104, -0.5805, -0.8319, -0.8952, -1.1107, -1.3188, -1.3396, -1.3817, -1.6797, -1.8333, -1.9543, -2.0837, -2.3423, -2.4561, -0.0258, -0.0927, -0.1121, -0.1324, -0.1242, -0.2126, -0.2561, -0.5839, -0.5881, -0.7285, -0.8235, -0.8304, -1.1211, -1.2100, -1.2906]
-    )
-
     sys.dgs = [
-        DistSystem.DG(18, 0.03, 0.0, [0.0026, 10.26, 210]),
-        DistSystem.DG(22, 0.03, 0.0, [0.0026, 10.26, 210]),
-        DistSystem.DG(25, 0.03, 0.0, [0.0026, 10.26, 210]),
-        DistSystem.DG(33, 0.03, 0.0, [0.0026, 10.26, 210])
+        DistSystem.DER(18, 0.02, 0.05, [0.0, 0.03], [-0.03, 0.03], [0.0, 1.0], [0.0026, 10.26, 210]), #DG dispatchable
+        DistSystem.DER(22, 0.02, 0.05, [0.0, 0.0], [-0.03, 0.03], [0.0, 1.0], [0.0026, 10.26, 210]), #DG non-active dispatchable
+        DistSystem.DER(33, 0.02, 0.15, [-0.01, 0.01], [-0.01, 0.01], [-1.0, 0.0, 1.0], [0.0026, 10.26, 210]), #ESS
+        DistSystem.DER(25, 0.02, 0.0, [0.0, 0.0], [0.0, 0.0], [0.0, 1.0], [0.0026, 10.26, 210]), #DG non-dispatchable
     ]
-    sys.m_load = collect(0.5:0.1:1.1)
-    sys.m_new_dg = collect(0.0:0.2:1.1)
-    return sys, case
+    sys.m_load = [0.5, 0.8, 1.0]
+    sys.m_new_dg = [-1.0, 0.0, 1]
+    return sys, "case33"
 end
 
 function test_add_variables(sot, sys)
